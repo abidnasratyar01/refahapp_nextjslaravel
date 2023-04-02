ac7b78b5566723483ef55252a45c92ab30c3b8b7
abidnasratyar01 <abidnasratyar01@gmail.com>
2023-04-02 21:13:32 +0430

    Started project task by creating laravel and next app.

---
+++ laravelrefah/app/Models/Refahuser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Refahuser extends Model
+{
+    use HasFactory;
+}
