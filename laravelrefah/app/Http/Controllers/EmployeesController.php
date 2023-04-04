d0b30a8188b9dfa299f69debd53699d3bf57703d
abidnasratyar01 <abidnasratyar01@gmail.com>
2023-04-04 21:58:52 +0430

    Crud operation Completed for Employees.

--- laravelrefah/app/Http/Controllers/EmployeesController.php
+++ laravelrefah/app/Http/Controllers/EmployeesController.php
@@ -20,13 +20,21 @@ class EmployeesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Employees::create([
+            'name' => $request->name,
+            'father_name' => $request->father_name,
+            'email' => $request->email,
+            'profile_dp' => $request->is_active,
+            'is_active' => $request->is_active
+        ]);
+
+        return response()->json(true);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Employee $employee)
+    public function show(Employees $employee)
     {
         //
     }
@@ -34,16 +42,25 @@ class EmployeesController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Employee $employee)
+    public function update(Request $request, Employees $employee)
     {
-        //
+        $employee->name = $request->name;
+        $employee->father_name = $request->father_name;
+        $employee->email = $request->email;
+        $employee->profile_dp = $request->is_active;
+        $employee->is_active = $request->is_active;
+        $employee->save();
+
+        return response()->json(true);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Employee $employee)
+    public function destroy(Employees $employee)
     {
-        //
+        $employee->delete();
+
+        return response()->json(true);
     }
 }
--- myrefahapp/pages/employees/employees.tsx
+++ myrefahapp/pages/employees/employees.tsx
@@ -9,6 +9,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Employees() {
   const [employees, setEmployess] = useState([]);
+  const [name, setName] = useState('');
+  const [fatherName, setFatherName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isActive, setIsActive] = useState('0');
+  const [employeeId, setEmployeeId] = useState('');
 
   useEffect(() => {
     fetchEmployess();
@@ -20,44 +25,101 @@ export default function Employees() {
     })
   }
 
+  const updateEmployee = (id:any) => {
+    setEmployeeId(id);
+    employees.map((item:any) => {
+      if (item.id == id) {
+        setName(item.name);
+        setFatherName(item.father_name);
+        setEmail(item.email);
+        setIsActive(item.is_active);
+      }
+    })
+  }
+
+  const deleteEmployee = (id:any) => {
+    let param = {'_method': 'delete'};
+    axios.post('api/employees/'+id, param).then((response:any) => {
+      fetchEmployess();
+    })
+  }
+
+  const submitForm = (e:any) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append('name',name);
+    formData.append('father_name', fatherName);
+    formData.append('email', email);
+    formData.append('is_active', isActive);
+
+    let url = 'api/employees';
+    if(employeeId != '') {
+      url = 'api/employees/'+employeeId;
+      formData.append('_method', 'PUT');
+    }
+
+    axios.post(url, formData).then((response:any) => {
+      document.forms[0].reset();
+      setName('');
+      setEmail('');
+      setIsActive('');
+      setFatherName('');
+      setEmployeeId('');
+      fetchEmployess();
+    })
+  }
+
   return (
     <>
       <Header title='Employees' />
       <main className={styles.main}>
-        <div className="container">  
-          <div><h1>Register Employee:</h1></div> 
-        </div>
-        <div className="container">
-          <form className='form-horizontal' action="" method="post" >
-            <div className="d-flex align-self-stretch">
-              <div className="mb-3">
-                <label>Name</label>
-                <input type="text" name="name" className="form-control" />
-              </div>
-              <div className="mb-3">
-                  <label>Father Name</label>
-                  <input type="text" name="father_name" className="form-control" />
-              </div>
-              <div className="mb-3">
-                  <label>Email</label>
-                  <input type="text" name="email" className="form-control" />
-              </div>
-           </div>
-           <div className="d-flex justify-content-between">
-              <div className="mb-3">
-                <label>Email</label>
-                <input type="text" name="email" className="form-control" />
-              </div>
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" name="active" value="" id="flexCheckDefault" />
-                <label className="form-check-label" >
-                  Active
-                </label>
-              </div>
-           </div>
-            <button type="submit" className="btn btn-primary" >Submit</button>
-          </form>
-        </div>
+
+        <div className="form-body">
+                <div className="row">
+                    <div className="form-holder">
+                        <div className="form-content">
+                            <div className="form-items">
+                                <h3>Register Employee</h3>
+                                <p>Fill in the data below.</p>
+                                <form className="requires-validation" method='POST' onSubmit={submitForm} id='employeeRegister'>
+                                    <div className="col-md-12">
+                                      <input className="form-control" type="text" name="name" placeholder="Full Name" value={name} required onChange={(e) => setName(e.target.value)} />
+                                      <div className="valid-feedback">Username field is valid!</div>
+                                      <div className="invalid-feedback">Username field cannot be blank!</div>
+                                    </div>
+
+                                    <div className="col-md-12">
+                                      <input className="form-control" type="text" name="father_name" placeholder="Father Name" value={fatherName} required onChange={(e) => setFatherName(e.target.value)}/>
+                                      <div className="valid-feedback">Username field is valid!</div>
+                                      <div className="invalid-feedback">Username field cannot be blank!</div>
+                                    </div>
+
+                                    <div className="col-md-12">
+                                        <input className="form-control" type="email" name="email" placeholder="E-mail Address" value={email} required onChange={(e) => setEmail(e.target.value)} />
+                                        <div className="valid-feedback">Email field is valid!</div>
+                                        <div className="invalid-feedback">Email field cannot be blank!</div>
+                                    </div>
+
+                                    <div className="col-md-12">
+                                        <label className="form-label">Profile DP</label>
+                                        <input type="file" className="form-control" id="customFile" />
+                                    </div>
+
+                                    <div className="form-check">
+                                      <input className="form-check-input" type="checkbox" name='is_active' value={isActive} id="invalidCheck" onChange={(e) => e.target.checked ? setIsActive('1') : setIsActive('0')}/>
+                                      <label className="form-check-label">Active</label>
+                                      <div className="invalid-feedback">Please confirm that the entered data are all correct!</div>
+                                    </div>
+                          
+                                    <div className="form-button mt-3">
+                                        <button id="submit" type="submit" className="btn btn-primary" >Register</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+          </div>
         <div className="container">  
           <div><h1>Employees Registered:</h1></div> 
         </div>
@@ -65,7 +127,7 @@ export default function Employees() {
           <table className="table table-hover table-dark">
             <thead>
                 <tr>
-                    <th scope='col'>EmpID</th>
+                    <th scope='col'>#</th>
                     <th scope='col'>Profile DP</th>
                     <th scope='col'>Name</th>
                     <th scope='col'>Father Name</th>
@@ -82,19 +144,11 @@ export default function Employees() {
                     <td>{item.name}</td>
                     <td>{item.father_name}</td>
                     <td>{item.email}</td>
-                    <td>{item.active}</td>
+                    <td>{item.is_active}</td>
                     <td>
                         <div className="d-flex justify-content-evenly">
-                            <form method="post" action="/controller=course&action=delete">
-                                <input type="hidden" name="id" className="form-control" value="" />
-                                <button type="submit" className="btn btn-danger">Delete</button>          
-                            </form>
-                            <form method="post" action="/controller=course&action=update">
-                                <input type="hidden" name="id" className="form-control" value="" />
-                                <input type="hidden" name="name" className="form-control" value="" />
-                                <input type="hidden" name="dept" className="form-control" value="" />
-                                <button type="submit" className="btn btn-primary">Update</button> 
-                            </form>
+                          <button type="submit" className="btn btn-danger" onClick={() => deleteEmployee(item.id)}>Delete</button>          
+                          <button type="submit" className="btn btn-primary" onClick={() => updateEmployee(item.id)}>Update</button> 
                         </div>
                     </td>
                 </tr>
